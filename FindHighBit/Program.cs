4749af2931ee9827d126f3fb7c4fbc18525eed56
Adrian Sims <adriansims62781@gmail.com>
2018-06-26 00:02:48 +0100

    Add project files.

---
+++ FindHighBit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindHighBit
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("You must have one argument.");
+                }
+                var filename = args[0];
+                var linenumber = 1;
+                foreach (var line in File.ReadAllLines(filename))
+                {
+                    var pos = 1;
+                    foreach (var c in line)
+                    {
+                        if (c > 0x80)
+                        {
+                            Console.WriteLine($"line is {linenumber}, pos is {pos}");
+                        }
+                        pos++;
+                    }
+                    linenumber++;
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add FindHighBit/Program.cs
+git add FindHighBit/App.config
+git add FindHighBit/Properties/AssemblyInfo.cs
+git add FindHighBit/FindHighBit.csproj
+git add FindHighBit.sln
+git add .gitignore
